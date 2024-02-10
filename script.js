6b7216d8098fd01042e1ba59d442728092d1bb51
AnselOngMJ <111511997+AnselOngMJ@users.noreply.github.com>
2024-02-10 11:24:28 +0000

    ????

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ function randomLetter() {
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     return letters[Math.floor(Math.random() * 25)];
 }
+var audio = new Audio('scream.mp3');
+
 
 function loadRandomLetter() {
     const letter = document.querySelector(".random-letter");
@@ -19,38 +21,38 @@ var dropAreaBin = document.getElementById('dropAreaBin');
 
 
 
-// Add data to the drag event
+
 draggableDiv.addEventListener('dragstart', function(e) {
-    e.dataTransfer.setData('text/plain', e.target.textContent);
+      e.dataTransfer.setData('text/plain', e.target.textContent);
 
 });
 
-// Prevent the default handling of the element
+
 dropArea.addEventListener('dragover', function(e) {
-    e.preventDefault(); // Necessary to allow the drop
+     e.preventDefault(); 
 });
 dropAreaBin.addEventListener('dragover', function(e) {
-    e.preventDefault(); // Necessary to allow the drop
-});
+    e.preventDefault(); 
+    });
 
-// Handle the drop action
+
 dropArea.addEventListener('drop', function(e) {
-    e.preventDefault(); // Prevent default action (open as link for some elements)
+    e.preventDefault(); 
     var data = e.dataTransfer.getData('text');
-    dropArea.value += data; // Append the text data into the textarea
+    dropArea.value += data; 
     loadRandomLetter();
 });
 
 dropAreaBin.addEventListener('drop', function(e) {
-    e.preventDefault(); // Prevent default action (open as link for some elements)
+    e.preventDefault();
     var data = e.dataTransfer.getData('text');
 
     loadRandomLetter();
+    audio.play();
 });
 
 function displayName() {
-    // document.querySelector("#name").textContent = 
-    // alert(`hi ${document.querySelector("textarea").value}`);
+
     alert('hi john');
 }
 document.querySelector('button').addEventListener('mouseover', () => {
