6e9a371d85b6819c6324e172f49b097a1fc8f541
AnselOngMJ <111511997+AnselOngMJ@users.noreply.github.com>
2024-02-10 11:30:40 +0000

    ?

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function randomLetter() {
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     return letters[Math.floor(Math.random() * 25)];
 }
-var audio = new Audio('scream.mp3');
+var audio = new Audio('static/scream.mp3');
 
 
 function loadRandomLetter() {
